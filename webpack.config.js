bce336c4475e7c08b62f78acc1addf1bde344e86
vcollado <necseron@gmail.com>
2017-07-29 15:42:21 +0200

    fixed deprecated plugin #5

--- webpack.config.js
+++ webpack.config.js
@@ -177,7 +177,7 @@ module.exports = function makeWebpackConfig() {
         config.plugins.push(
                 // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
                 // Only emit files when there are no errors
-                new webpack.NoErrorsPlugin(),
+                new webpack.NoEmitOnErrorsPlugin(),
                 // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
                 // Minify all javascript, switch loaders to minimizing mode
                 new webpack.optimize.UglifyJsPlugin({
